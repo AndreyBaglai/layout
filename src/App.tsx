9226c4d56a9fd9b183c2bcc693cc71bda6818d6b
AndreyBaglai <andreybaglai@gmail.com>
2021-09-23 11:30:42 +0300

    feat: add basic logic for change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello</div>;
+  const onClick = () => {
+    document.body.classList.toggle('dark');
+  };
+
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae in eius, quas dolores
+        non esse, eaque minus accusantium totam, explicabo impedit suscipit asperiores quasi modi
+        commodi cupiditate? Velit, accusamus illo?
+      </p>
+      <button onClick={onClick}>Switch theme</button>
+    </div>
+  );
 };
 
 export default App;
