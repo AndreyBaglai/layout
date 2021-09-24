a37a99ac3116da88c3c333d68e8277af78046bb4
AndreyBaglai <andreybaglai@gmail.com>
2021-09-24 10:07:26 +0300

    feat: add categories component

---
+++ src/containers/Page/Categories/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import SectionHeader from 'components/SectionHeader';
+import InputRange from 'components/InputRange';
+
+import categoriesSrc1 from 'sources/images/categories-1.png';
+import categoriesSrc2 from 'sources/images/categories-2.png';
+import categoriesSrc3 from 'sources/images/categories-3.png';
+
+import styles from './styles.module.scss';
+import Card from 'components/Card';
+
+const Categories: React.FC = () => {
+  return (
+    <section className={styles.categories}>
+      <SectionHeader title="Categories" controls={true} />
+      <div className={styles.content}>
+        {' '}
+        <div className={styles.mainImg}>
+          <Card size="big" imageSrc={categoriesSrc1} text="Women" />
+        </div>
+        <div className={styles.items}>
+          <Card size="small" imageSrc={categoriesSrc2} text="Kids" />
+          <Card size="small" imageSrc={categoriesSrc3} text="Men" />
+        </div>
+      </div>
+
+      <InputRange />
+    </section>
+  );
+};
+
+export default Categories;
--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -6,13 +6,12 @@ import Divider from 'components/Divider';
 
 import imageSrc1 from 'sources/images/top-bg.png';
 import imageSrc2 from 'sources/images/bottom-bg.png';
-import categoriesSrc1 from 'sources/images/categories-1.png';
-import categoriesSrc2 from 'sources/images/categories-2.png';
 import dontMissSrc1 from 'sources/images/dont-miss-1.png';
 
 import InputRange from 'components/InputRange';
 import SectionHeader from 'components/SectionHeader';
 import Card from 'components/Card';
+import Categories from './Categories';
 
 const Page: React.FC = () => {
   // const onClick = () => {
@@ -29,11 +28,9 @@ const Page: React.FC = () => {
         imageBg={imageSrc1}
       />
       <Divider text="We’ve extended our returns period to 60 days" />
-      {/* <InputRange />
-      <SectionHeader title="Trending Now" controls={true} />
+      {/*
       <SectionHeader title="Don't miss" controls={false} /> */}
-      <Card size="big" imageSrc={categoriesSrc1} text="Women" />
-      <Card size="small" imageSrc={categoriesSrc2} text="Kids" />
+      <Categories />
       <Card size="middle" imageSrc={dontMissSrc1} text="Special Offer" />
       <Sample
         title="New in Town"
