40204dcccf4ab852664f42c1dec172c84c47eae2
AndreyBaglai <andreybaglai@gmail.com>
2021-09-27 14:00:26 +0300

    fix: input type range styles

--- src/components/InputRange/index.tsx
+++ src/components/InputRange/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 const InputRange: React.FC = () => (
-  <div>
+  <div className={styles.inputWrapper}>
     <input className={styles.range} type="range" min="1" max="2" />
   </div>
 );
