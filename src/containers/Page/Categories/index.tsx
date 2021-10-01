a4a34b10449839a4708811db48fa0066a3df9052
AndreyBaglai <andreybaglai@gmail.com>
2021-10-01 12:46:09 +0300

    refactor: categories component

--- src/containers/Page/Categories/index.tsx
+++ src/containers/Page/Categories/index.tsx
@@ -1,58 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import Slider from 'react-slick';
 
 import InputRange from 'components/InputRange';
 import Card from 'components/Card';
+import SampleNextArrow from 'components/SliderArrows/Next';
+import SamplePrevArrow from 'components/SliderArrows/Prev';
+
+import { ReactComponent as MobileRightArrowIcon } from 'sources/icons/mobile-arrow-icon.svg';
 
 import categoriesSrc1 from 'sources/images/categories-1.png';
 import categoriesSrc2 from 'sources/images/categories-2.png';
 import categoriesSrc3 from 'sources/images/categories-3.png';
-import { ReactComponent as MobileRightArrowIcon } from 'sources/icons/mobile-arrow-icon.svg';
+
+import { CATEGORIES_SLIDER_MOBILE_WIDTH, CLIENT_WIDTH } from 'utils/const';
 
 import styles from './styles.module.scss';
-import SampleNextArrow from 'components/SliderArrows/Next';
-import SamplePrevArrow from 'components/SliderArrows/Prev';
-import Slider from 'react-slick';
+
+let sliderCategories: Slider | null;
 
 const Categories: React.FC = () => {
   const [currIdx, setCurrIdx] = useState(0);
-  const [maxInput, setMaxInput] = useState(1);
-  
+  const [maxInputScroll, setMaxInputScroll] = useState(1);
+
   useEffect(() => {
-    if (document.documentElement.clientWidth < 769) {
-      setMaxInput(2);
+    if (CLIENT_WIDTH < CATEGORIES_SLIDER_MOBILE_WIDTH) {
+      setMaxInputScroll(2);
     }
   }, []);
 
-  const onChangeSlider = ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
-    sliderCategories.slickGoTo(currentTarget.value);
-  };
+  const onChangeSlider = useCallback(
+    ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
+      sliderCategories?.slickGoTo(Number(currentTarget.value));
+    },
+    [currIdx],
+  );
 
-  let sliderCategories: any;
-
-  const settings = {
-    rows: 2,
-    variableWidth: false,
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    accessibility: true,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    beforeChange: (_: any, next: any) => setCurrIdx(next),
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 0,
+  const settingsCategoriesSlider = useMemo(
+    () => ({
+      rows: 2,
+      variableWidth: false,
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      accessibility: true,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      beforeChange: (_: any, nextIdx: number) => setCurrIdx(nextIdx),
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 0,
+          },
         },
-      },
-    ],
-  };
+      ],
+    }),
+    [currIdx],
+  );
 
   return (
     <section className={styles.categories}>
@@ -61,7 +70,7 @@ const Categories: React.FC = () => {
         <MobileRightArrowIcon className={styles.mobileArrow} />
       </h2>
 
-      <Slider ref={(slider) => (sliderCategories = slider)} {...settings}>
+      <Slider ref={(slider: Slider) => (sliderCategories = slider)} {...settingsCategoriesSlider}>
         <div id="bigImg" className={styles.bigImg}>
           <Card size="big" imageSrc={categoriesSrc1} text="Women" />
         </div>
@@ -73,7 +82,7 @@ const Categories: React.FC = () => {
         <Card size="small" imageSrc={categoriesSrc2} text="Kids" />
       </Slider>
 
-      <InputRange max={maxInput} onChange={onChangeSlider} sizeThumb="big" value={currIdx} />
+      <InputRange max={maxInputScroll} onChange={onChangeSlider} sizeThumb="big" value={currIdx} />
     </section>
   );
 };
